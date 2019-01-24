cd3e5b36d9578db2d8c3874e79e6fe8e9bb950a4
ekicyou <dot.station@gmail.com>
2019-01-24 15:53:30 +0900

    update

--- src/api/api.rs
+++ src/api/api.rs
@@ -9,3 +9,13 @@ pub trait Shiori3: Drop + Sized {
     /// SHIORIリクエストを解釈し、応答を返します。
     fn request<'a, S: Into<&'a str>>(&mut self, req: S) -> Result<Cow<'a, str>>;
 }
+
+pub trait Shiori3X: Drop + Sized {}
+
+pub trait IsSvcShiori3: Drop + Sized {
+    /// 新しいSHIORIインスタンスを作成します。
+    fn load<P: AsRef<Path>>(h_inst: usize, load_dir: P) -> Result<Self>;
+
+    /// SHIORIリクエストを解釈し、応答を返します。
+    fn request<'a, S: Into<&'a str>>(&mut self, req: S) -> Result<Cow<'a, str>>;
+}
---
+++ src/api/readme.md
@@ -0,0 +1,13 @@
+rust shiori API仕様
+===================
+
+1. 実装の取得
+
+S
+
+
+
+
+
+Shiori3
+IsSrvShiori3
