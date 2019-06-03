d0e92a911d2fe2fb9137df6cdd9d57382eb2ac41
ekicyou <dot.station@gmail.com>
2019-06-03 16:26:44 +0900

    update

--- src/shiori.rs
+++ src/shiori.rs
@@ -9,7 +9,7 @@ use std::path::PathBuf;
 #[derive(Default)]
 pub struct Shiori {
     h_inst: usize,
-    load_dir: PathBuf,
+    ansi_load_dir: PathBuf,
     lua_path: String,
     lua: Lua,
 }
@@ -22,8 +22,8 @@ impl Shiori {
     fn h_inst(&self) -> usize {
         (self.h_inst)
     }
-    fn load_dir(&self) -> &Path {
-        &(self.load_dir)
+    fn ansi_load_dir(&self) -> &Path {
+        &(self.ansi_load_dir)
     }
     fn lua(&self) -> &Lua {
         &(self.lua)
@@ -32,9 +32,9 @@ impl Shiori {
 
 impl Shiori3 for Shiori {
     /// load_dir pathのファイルでSHIORIインスタンスを作成します。
-    fn load<P: AsRef<Path>>(h_inst: usize, load_dir: P) -> Result<Self, failure::Error> {
+    fn load<P: AsRef<Path>>(h_inst: usize, ansi_load_dir: P) -> Result<Self, failure::Error> {
         // 検索パスの作成
-        let (load_dir, lua_path) = lua_search_path(load_dir, "lua")?;
+        let (ansi_load_dir, lua_path) = lua_search_path(ansi_load_dir, "lua")?;
 
         // ##  Lua インスタンスの作成
         let lua = Lua::new();
@@ -68,7 +68,7 @@ impl Shiori3 for Shiori {
         // リザルト
         Ok(Shiori {
             h_inst: h_inst,
-            load_dir: load_dir,
+            ansi_load_dir: ansi_load_dir,
             lua_path: lua_path,
             lua: lua,
         })
@@ -78,81 +78,7 @@ impl Shiori3 for Shiori {
     fn request<'a, S: Into<&'a str>>(&mut self, req: S) -> Result<Cow<'a, str>, failure::Error> {
         let req_str = req.into();
         let _req = ShioriRequest::parse(req_str)?;
-        let rc = format!("[{:?}]{} is OK", self.load_dir, req_str);
+        let rc = format!("[{:?}]{} is OK", self.ansi_load_dir, req_str);
         Ok(rc.into())
-    }
-}
-
-/// luaの検索パスを作成します。
-fn lua_search_path<P: AsRef<Path>>(
-    load_dir: P,
-    ext: &str,
-) -> Result<(PathBuf, String), failure::Error> {
-    // load dir(終端文字は除去)
-    let load_dir = {
-        let mut buf = load_dir.as_ref().to_path_buf();
-        buf.push("a");
-        buf.pop();
-        buf
-    };
-
-    // クロージャ呼び出し
-    let mut lua_path = String::default();
-    {
-        let load_dir = {
-            let a = load_dir.to_str();
-            let b = a.ok_or(ShioriError::from(ShioriErrorKind::Load))?;
-            String::from(b)
-        };
-        // luaモジュールのパス解決関数
-        let mut add_path = |root: &str| {
-            let mut add_path = |pre: &str| {
-                if !lua_path.is_empty() {
-                    lua_path.push(';');
-                }
-                lua_path.push_str(&load_dir);
-                lua_path.push_str(root);
-                lua_path.push_str(pre);
-                lua_path.push_str(ext);
-            };
-            add_path("\\?.");
-            add_path("\\?\\init.");
-        };
-        add_path("\\usr");
-        add_path("\\share");
-    }
-
-    Ok((load_dir, lua_path))
-
-    // # luaのモジュール解決
-    // modname は以下の順に検索され、最初に解決したものを返す。
-    // この動作はpackage.searchersに登録されている既定の動作である。
-    //  1. package.preload[modname]
-    //  2. package.path  を用いてパス解決
-    //  3. package.cpath を用いてパス解決
-    //  4. package.searchers より、オールインワンローダで解決
-
-    // ## パス解決ルール
-    // パス解決は package.searchpath (name, path [, sep [, rep]]) により行われる。
-    // パスに含まれる「?」が、nameに置き換わり、最初に見つかったファイル名を返す。
-    // 例）
-    //   * name ="foo.a"
-    //     * ? ="foo/a"
-    //   * path ="./?.lua;./?.lc;/usr/local/?/init.lua"
-    //     * "./foo/a.lua"
-    //     * "./foo/a.lc"
-    //     * "/usr/local/foo/a/init.lua"
-
-    // ## moe.dllのパス解決
-    //  1. load_dirを絶対パスに変換する。例）load_dir="c:\萌え 萌え\ghost\キュン";
-    //  2.
-}
-#[cfg(any(windows))]
-#[test]
-fn lua_search_pathh_test() {
-    {
-        let (dir, path) = lua_search_path("c:\\留袖 綺麗ね\\ごーすと\\", "lua").unwrap();
-        assert_eq!(dir.to_string_lossy(), "c:\\留袖 綺麗ね\\ごーすと");
-        assert_eq!(path, "c:\\留袖 綺麗ね\\ごーすと\\usr\\?.lua;c:\\留袖 綺麗ね\\ごーすと\\usr\\?\\init.lua;c:\\留袖 綺麗ね\\ごーすと\\share\\?.lua;c:\\留袖 綺麗ね\\ごーすと\\share\\?\\init.lua");
     }
 }
