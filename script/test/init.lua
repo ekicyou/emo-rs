3f8797dc515e775ddf48926205aa1976b7198109
ekicyou <dot.station@gmail.com>
2019-06-24 16:58:48 +0900

    update

--- script/test/init.lua
+++ script/test/init.lua
@@ -1,5 +1,12 @@
 require "test.shiori.response"
 
+function test_SaveLoad()
+    local ser = require "libs.serpent"
+
+
+end
+
+
 local t = require "test.luaunit"
 t.set_verbosity(t.VERBOSITY_VERBOSE)
 local rc = t.run()
