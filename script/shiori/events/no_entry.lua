0678f6082f7a537dbe511ab9dc2c71450522b6d8
ekicyou <dot.station@gmail.com>
2019-07-09 09:35:29 +0900

    試験更新

--- script/shiori/events/no_entry.lua
+++ script/shiori/events/no_entry.lua
@@ -4,11 +4,20 @@ local utils     = require "shiori.utils"
 return function(EV)
 --
 function EV:no_entry(env, req)
-    -- 初めての未処理リクエストなら保存
     local id = req.id
+
+    -- notify なら保存
+    if req.method == "notify" then
+        local a = utils.get_tree_entry(env ,"notify")
+        a[id] = req
+        return response.no_content()
+    end
+
+    -- 初めての未処理リクエストなら保存
     local a = utils.get_tree_entry(env ,"save", "no_entry")
     if a[id] == nil then a[id] = req
     end
+
     -- 応答
     local opt = {["X-Warn-Resion"]= "no_entry"}
     return response.no_content(opt)
--- script/test/init.lua
+++ script/test/init.lua
@@ -24,6 +24,36 @@ function test_shiori_event()
         t.assertEquals(act, exp)
         t.assertNotIsNil(env.save.no_entry.version)
     end
+    do
+        local args = {}
+        args[0] = ""
+        local req = make.notify("OnInitialize", args)
+        local act = ev:fire_request(env,req)
+        local exp = X(  "SHIORI/3.0 204 No Content",
+                        "Charset: UTF-8",
+                        "Sender: emo",
+                        "SecurityLevel: local")
+        t.assertEquals(act, exp)
+        t.assertEquals(env.notify.OnInitialize.reference[0], "")
+        t.assertIsNil(env.notify.OnInitialize.reference[1])
+    end
+    do
+        local args = {}
+        args[0] = "2.3.86"
+        args[1] = "SSP"
+        args[2] = "2.3.86.3000"
+        local req = make.notify("basewareversion", args)
+        local act = ev:fire_request(env,req)
+        local exp = X(  "SHIORI/3.0 204 No Content",
+                        "Charset: UTF-8",
+                        "Sender: emo",
+                        "SecurityLevel: local")
+        t.assertEquals(act, exp)
+        t.assertEquals(env.notify.basewareversion.reference[0], "2.3.86")
+        t.assertEquals(env.notify.basewareversion.reference[1], "SSP")
+        t.assertEquals(env.notify.basewareversion.reference[2], "2.3.86.3000")
+        t.assertIsNil(env.notify.basewareversion.reference[3])
+    end
 
 end
 
