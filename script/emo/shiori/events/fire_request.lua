58435024d0ed7b681a4d4bb29b8fae3aec4c6cdb
ekicyou <dot.station@gmail.com>
2019-07-19 14:06:43 +0900

    req.dateを登録

--- script/emo/shiori/events/fire_request.lua
+++ script/emo/shiori/events/fire_request.lua
@@ -7,6 +7,10 @@ function EV:fire_request(data, req)
     -- Statusがある場合の分解
     local status_dic = utils.get_status(req.status)
     req.status_dic = status_dic
+    -- 日時フィールドが未登録なら登録
+    if req.date == nil then
+        req.date = os.date("*t")
+    end
 
     -- イベント分岐
     local id = req.id
--- script/test/init.lua
+++ script/test/init.lua
@@ -2,39 +2,16 @@ require "test.utils.save_load"
 require "test.shiori.response"
 require "test.shiori.event"
 require "test.shiori.utils"
+require "test.shiori.load"
 
 
 -- とりあえずすぐ試したいテストはここに書く。
-function test_every()
+function test_os_date()
     local t = require "test.luaunit"
-    local utils = require "shiori.utils"
     local ser = require "libs.serpent"
-    local shiori = require "shiori"
-    local response = require "shiori.response"
-    local make = require "test.shiori.make_request"
-    local X = response.join
 
-    local hinst = 1234
-    local WORK_SPACE_DOLDER = os.getenv('WORK_SPACE_DOLDER')
-    local ansi_load_dir = WORK_SPACE_DOLDER .. '\\script'
-
-    do
-        local rc = shiori.load(hinst, ansi_load_dir)
-        t.assertIsTrue(rc)
-    end
-    do
-        local args = {}
-        args[0] = "マスターシェル"
-        local req = make.get("OnBoot", args)
-        local res = shiori.request(req)
-        local exp = X(  "SHIORI/3.0 200 OK",
-                        "Charset: UTF-8",
-                        [=[Value: \1\s[10]\0\s[0]OnBoot:起動トークです。\e]=])
-        t.assertEquals(res, exp)
-    end
-    do
-        local rc = shiori.unload()
-    end
+    local d = os.date("*t")
+    t.assertIsTrue(d.year > 2018)
 end
 
 -- ここまで
