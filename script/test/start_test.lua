e327970f4583fc964be82cc33dd67e5a5b705416
ekicyou <dot.station@gmail.com>
2021-04-19 17:44:56 +0900

    調査中

--- script/test/start_test.lua
+++ script/test/start_test.lua
@@ -7,6 +7,22 @@ function test_hello(d)
     local t = require "test.luaunit"
     local x = "hello"
     t.assertEquals(x,"hello")
+    t.assertEquals(_VERSION,"Lua 5.1")
+end
+
+
+function test_utf8(d)
+    local t = require "test.luaunit"
+    local utf8 = require("libs.utf8"):init()
+    local str = "пыщпыщ ололоо я водитель нло"
+    print(str:find("(.л.+)н"))
+    -- 8	26	ололоо я водитель
+
+    local rep, cnt = str:gsub("ло+", "보라")
+    t.assertEquals(rep, "пыщпыщ о보라보라о я водитель н보라")
+    t.assertEquals(rep, 3)
+
+    t.assertEquals(str:match("^п[лопыщ ]*я"), "пыщпыщ ололоо я")
 
 end
 
