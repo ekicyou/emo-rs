c095a7b13c0178bdcb758f926312c21b31322bcf
ekicyou <dot.station@gmail.com>
2023-03-23 14:05:44 +0900

    ss.talk

--- script.new2/test/start_test.lua
+++ script.new2/test/start_test.lua
@@ -4,6 +4,23 @@ require "test"
 
 -- とりあえずすぐ試したいテストはここに書く。
 
+function test_ss_talk()
+    local t = require "test.luaunit"
+    local ss = require "emo.dic.sakura_script"
+
+    local wait = {
+        050, -- 通常wait
+        300, -- 半濁点
+        432, -- エクステンション
+        600, -- 濁点
+        150, -- 点々
+    }
+    local text = [=[今日は、いい天気？だめだね。改行‥‥\n[123]だよ。]=]
+    local ext = [=[今日は、\w6いい天気？\_w[432]だめだね。\_w[600]改行‥\w2‥\w2\n[123]だよ。\_w[600]]=]
+    local act = ss.talk(wait, text)
+    t.assertEquals(act, ext)
+end
+
 function test_dic_entry()
     local t = require "test.luaunit"
     local entry = require "emo.dic.entry"
