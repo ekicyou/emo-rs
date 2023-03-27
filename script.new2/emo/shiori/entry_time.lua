0cc2223d7aa7bd7f3acd06648e7ea461073dead4
ekicyou <dot.station@gmail.com>
2023-03-27 15:30:48 +0900

    test

--- script.new2/emo/shiori/entry_time.lua
+++ script.new2/emo/shiori/entry_time.lua
@@ -1,20 +1,19 @@
 -- カレンダートークのエントリー処理
 local MOD = {}
 
-
-local RE_CAL_DY = "D([%d%-][%d%-])([%d%-][%d%-])([%d%-][%d%-])"
-local RE_CAL_DM = "D([%d%-][%d%-])([%d%-][%d%-])"
-local RE_CAL_W = "W(%d+)"
-local RE_CAL_T = "T([%d%-][%d%-])([%d%-][%d%-])"
-local RE_NUM = "^(%d+)$"
-
-
 --- osdate `os.date('*t',time) を返す。
 --- @param time integer os.time()値
 --- @return osdate date `os.date('*t',time)値
 function MOD.osdate(time)
     return os.date("*t", time)
 end
+
+local RE_CAL_DY = "D([%d%-][%d%-])([%d%-][%d%-])([%d%-][%d%-])"
+local RE_CAL_DM = "D([%d%-][%d%-])([%d%-][%d%-])"
+local RE_CAL_W = "W(%d+)"
+local RE_CAL_T = "T([%d%-][%d%-])([%d%-][%d%-])"
+local RE_CAL_M = "M([%d%-][%d%-])"
+local RE_NUM = "^(%d+)$"
 
 --- エントリをテーブル分解。
 --- @param entry string 日時エントリー定義文字列。`Dyymmdd` 年月日、`Dmmdd`月日、`Wn`週（日曜日=1始まり）、`Thhmm`日時
@@ -30,29 +29,41 @@ function MOD.date_table(entry)
     end
 
     local x = {}
-    local s, _, year, month, day = string.find(entry, RE_CAL_DY)
-    if s then
-        x.year  = NUM(year)
-        x.month = NUM(month)
-        x.day   = NUM(day)
-        if x.year then
-            x.year = x.year + 2000
-        end
-    else
-        local s, _, month, day = string.find(entry, RE_CAL_DM)
+    do
+        local s, _, year, month, day = string.find(entry, RE_CAL_DY)
         if s then
+            x.year  = NUM(year)
             x.month = NUM(month)
             x.day   = NUM(day)
+            if x.year then
+                x.year = x.year + 2000
+            end
+        else
+            s, _, month, day = string.find(entry, RE_CAL_DM)
+            if s then
+                x.month = NUM(month)
+                x.day   = NUM(day)
+            end
         end
     end
-    local s, _, a = string.find(entry, RE_CAL_W)
-    if s then
-        x.wday = NUM(a)
+    do
+        local s, _, a = string.find(entry, RE_CAL_W)
+        if s then
+            x.wday = NUM(a)
+        end
     end
-    local s, _, hour, min = string.find(entry, RE_CAL_T)
-    if s then
-        x.hour = NUM(hour)
-        x.min  = NUM(min)
+    do
+        local s, _, hour, min = string.find(entry, RE_CAL_T)
+        if s then
+            x.hour = NUM(hour)
+            x.min  = NUM(min)
+        end
+    end
+    do
+        local s, _, min = string.find(entry, RE_CAL_M)
+        if s then
+            x.min = NUM(min)
+        end
     end
     return x
 end
@@ -76,21 +87,8 @@ function MOD.create_entry(entry)
     return t
 end
 
---- 与えられた時刻に対するエントリマッチ度を返す。
---- @param entry table エントリテーブル
---- @param now osdate 比較する時刻テーブル
---- @return number|nil priority マッチした場合、priority値。マッチしなければnil
-function MOD.entry_match(entry, now)
-    for k, v in pairs(entry.date) do
-        if now[k] ~= v then
-            return nil
-        end
-    end
-    return entry.priority + entry.rand
-end
-
 --- 指定日時をベースに直近の月曜日午前０時を返す。
---- @param target table 基準日時
+--- @param target osdate 基準日時
 --- @return number monday_time 直前の月曜日午前０時
 function MOD.get_last_monday_time(target)
     local d2 = {
@@ -111,6 +109,186 @@ function MOD.get_last_monday_time(target)
     if day < 0 then day = day + 7 end
     local monday_time = zero_time - day * 60 * 60 * 24
     return monday_time
+end
+
+local TIME_1MIN  = 60
+local TIME_1HOUR = TIME_1MIN * 60
+local TIME_1DAY  = TIME_1HOUR * 24
+local TIME_1WEEK = TIME_1DAY * 7
+
+--- 日時エントリー定義と現在時刻より、直近の発動時刻を返す。
+--- @param entry osdate エントリー定義
+--- @param now_time integer 参照時刻
+--- @param now osdate 参照時刻
+--- @return integer|nil s_time 発動開始時刻
+--- @return integer|nil e_time 発動終了時刻（同じ値は含まない）
+function MOD.calc_fire_time(entry, now_time, now)
+    local year = entry.year
+    local month = entry.month
+    local day = entry.day
+    local wday = entry.wday
+    local hour = entry.hour
+    local min = entry.min
+
+    local function TIME_DUE()
+        if min then
+            return TIME_1MIN
+        elseif hour then
+            return TIME_1HOUR
+        end
+        return TIME_1DAY
+    end
+    local due = TIME_DUE()
+
+    if hour == nil then
+        hour = 0
+        min = 0
+    elseif min == nil then
+        min = 0
+    end
+
+    local function S_YEAR()
+        local t = {
+            year  = year,
+            month = month,
+            day   = day,
+            hour  = hour,
+            min   = min,
+        }
+        return os.time(t)
+    end
+
+    -- 年指定
+    if year then
+        local s_time = S_YEAR()
+        local e_time = s_time + due
+        local s = MOD.osdate(s_time)
+        local e = MOD.osdate(e_time)
+        if e_time > now_time then
+            return s_time, e_time
+        else
+            return nil
+        end
+    end
+
+    -- 月指定
+    if month then
+        local s = {
+            year  = now.year,
+            month = month,
+            day   = day,
+            hour  = hour,
+            min   = min,
+        }
+        local s_time = os.time(s)
+        local e_time = s_time + due
+        if e_time > now_time then
+            -- e_timeが未来日時ならそのまま
+            return s_time, e_time
+        end
+        -- e_timeが過去日時だったので１年進める
+        s.year = s.year + 1
+        s_time = os.time(s)
+        e_time = s_time + due
+        return s_time, e_time
+    end
+
+    -- 週指定
+    local function WEEK_SKIP()
+        local rc = wday - 2
+        if rc < 0 then
+            rc = rc + 7
+        end
+        return rc
+    end
+
+    local function HOUR_TIME()
+        if not hour then return 0 end
+        return TIME_1HOUR * hour + TIME_1MIN * min
+    end
+
+    -- 最初に一致する現在または未来の週日時
+    if wday then
+        local function FOREACH()
+            local monday_time = MOD.get_last_monday_time(now) + HOUR_TIME()
+            local skip_time = TIME_1DAY * WEEK_SKIP()
+            local time = monday_time + skip_time
+            return function()
+                local s_time = time
+                local e_time = s_time + due
+                time = time + TIME_1WEEK
+                return s_time, e_time
+            end
+        end
+        for s_time, e_time in FOREACH() do
+            if e_time > now_time then
+                return s_time, e_time
+            end
+        end
+    end
+
+    -- 時刻の一致
+    if hour then
+        local function FOREACH()
+            local s = {
+                year  = now.year,
+                month = now.month,
+                day   = now.day,
+                hour  = hour,
+                min   = min,
+            }
+            local time = os.time(s)
+            return function()
+                local s_time = time
+                local e_time = s_time + due
+                time = time + TIME_1DAY
+                return s_time, e_time
+            end
+        end
+        for s_time, e_time in FOREACH() do
+            if e_time > now_time then
+                return s_time, e_time
+            end
+        end
+    end
+
+    -- 分の一致
+    do
+        local function FOREACH()
+            local s = {
+                year  = now.year,
+                month = now.month,
+                day   = now.day,
+                hour  = now.hour,
+                min   = min,
+            }
+            local time = os.time(s)
+            return function()
+                local s_time = time
+                local e_time = s_time + due
+                time = time + TIME_1HOUR
+                return s_time, e_time
+            end
+        end
+        for s_time, e_time in FOREACH() do
+            if e_time > now_time then
+                return s_time, e_time
+            end
+        end
+    end
+end
+
+--- 与えられた時刻に対するエントリマッチ度を返す。
+--- @param entry table エントリテーブル
+--- @param now osdate 比較する時刻テーブル
+--- @return number|nil priority マッチした場合、priority値。マッチしなければnil
+function MOD.entry_match(entry, now)
+    for k, v in pairs(entry.date) do
+        if now[k] ~= v then
+            return nil
+        end
+    end
+    return entry.priority + entry.rand
 end
 
 -- 全エントリの発動時刻を削除する
--- script.new2/test/start_test.lua
+++ script.new2/test/start_test.lua
@@ -4,9 +4,38 @@ require "test"
 
 -- とりあえずすぐ試したいテストはここに書く。
 
-function test_entry_time_fire_entry()
+function test_entry_time_calc_fire_time()
     local t = require "test.luaunit"
     local et = require "shiori.entry_time"
+    local now_time = os.time(et.date_table("D211231T0123"))
+    local now = et.osdate(now_time)
+    local function TRIM(s)
+        return (s:gsub("^%s*(.-)%s*$", "%1"))
+    end
+    local function X(entry, ext_s, ext_e)
+        local table = et.date_table(TRIM(entry))
+        local s, e = et.calc_fire_time(table, now_time, now)
+        if s == nil and ext_s == nil then return end
+        local act_s = os.date("%Y%m%d_%H%M%S", s)
+        local act_e = os.date("%Y%m%d_%H%M%S", e)
+        t.assertEquals(act_s, ext_s, "s")
+        t.assertEquals(act_e, ext_e, "e")
+    end
+    --"D211231T0123"
+    X("D211231T0123", "20211231_012300", "20211231_012400")
+    X("D211231T0159", "20211231_015900", "20211231_020000")
+    X("D211231     ", "20211231_000000", "20220101_000000")
+    X("D211231T0122", nil)
+    X("D211230     ", nil)
+    X("D1231       ", "20211231_000000", "20220101_000000")
+    X("D1230       ", "20221230_000000", "20221231_000000")
+    X("W1          ", "20220102_000000", "20220103_000000")
+    X("W2          ", "20220103_000000", "20220104_000000")
+    X("W3          ", "20220104_000000", "20220105_000000")
+    X("W4          ", "20220105_000000", "20220106_000000")
+    X("W5          ", "20220106_000000", "20220107_000000")
+    X("W6          ", "20211231_000000", "20220101_000000")
+    X("W7          ", "20220101_000000", "20220102_000000")
 end
 
 function test_dic_entry()
