7397d712c345924581e6306341a2e497a8127779
ekicyou <dot.station@gmail.com>
2023-03-30 17:07:16 +0900

    現状でコミット

---
+++ script.new2/emo/emo/dic/util.lua
@@ -0,0 +1,49 @@
+-- ユーティリティ関数
+local MOD = {}
+
+function MOD.random(...)
+    return math.random(...)
+end
+
+--- ランダムトーク関数を作成する。
+--- @vararg function トーク関数
+--- @return function randam_talk ランダムトーク関数
+function MOD.select_talk_builder(...)
+    local array = { ... }
+    if #array == 0 then
+        error("#{...} is 0")
+    end
+    return function(...)
+        local random_index = MOD.random(#array)
+        local random_element = array[random_index]
+        return random_element(...)
+    end
+end
+
+--- 要素をshuffleして列挙し続ける関数を作成する。
+--- @vararg any アイテム
+--- @return function gen アイテムをshuffleして返し続ける列挙関数
+function MOD.gen_shuffle_builder(...)
+    local array = { ... }
+    if #array == 0 then
+        error("#{...} is 0")
+    end
+
+    local function GEN()
+        ::START::
+        local shuffled_array = {}
+        for i = 1, #array do
+            local pos = MOD.random(1, i)
+            shuffled_array[i] = shuffled_array[pos]
+            shuffled_array[pos] = array[i]
+        end
+        for i = 1, #shuffled_array do
+            coroutine.yield(shuffled_array[i])
+        end
+        goto START
+    end
+
+    return coroutine.wrap(GEN)
+end
+
+return MOD
--- script.new2/test/test/dic.lua
+++ script.new2/test/test/dic.lua
@@ -24,3 +24,38 @@ function test_dic_actor_register()
     t.assertEquals(items[1],
         [[\1\s[0]\0\s[0]ねぇ、\w6紫ちゃん。\_w[500]今日は何の日かな？\w8\1\s[10]今日は木曜日だよ。\_w[500]\0\n[150]\s[7]何か楽しいことあるよね！\w8\1\n[150]そんな予定はないね。\_w[500]\0\n[150]\s[3]えー‥\w3‥\w3‥\w3\e]])
 end
+
+function test_dic_util_random_selector()
+    require "test.test_actor"
+    local util = require "emo.dic.util"
+    local t = require "test.luaunit"
+
+    local function a(scene) return 'a' end
+    local function b(scene) return 'b' end
+
+    local sel = util.select_talk_builder(a, b)
+    local scene = {}
+
+    util.random = function() return 1 end
+    t.assertEquals(sel(scene), 'a')
+    util.random = function() return 2 end
+    t.assertEquals(sel(scene), 'b')
+end
+
+function test_dic_util_gen_shuffle_builder()
+    require "test.test_actor"
+    local util = require "emo.dic.util"
+    local t = require "test.luaunit"
+
+    local a = 1
+    local b = 2
+    local c = 3
+    util.random = function(x, y) return y end
+    local gen = util.gen_shuffle_builder(a, b, c)
+    t.assertEquals(gen(), 1)
+    t.assertEquals(gen(), 2)
+    t.assertEquals(gen(), 3)
+    t.assertEquals(gen(), 1)
+    t.assertEquals(gen(), 2)
+    t.assertEquals(gen(), 3)
+end
